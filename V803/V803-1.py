d52d51b4476952ed9508164e41a91bfd1815de20
Tobias Rücker <tobias.ruecker@tu-dortmund.de>
2019-11-01 16:34:02 +0100

    adding variables from measure

--- V803/V803-1.py
+++ V803/V803-1.py
@@ -3,28 +3,31 @@ import numpy as np
 import uncertainties.unumpy as unp
 from uncertainties.unumpy import (
     nominal_values as noms,
-    std_devs as stds,
+    std_devs as stds
+)
 
-t, U, U_err = np.genfromtxt('data.txt', unpack=True)
-t *= 1e-3
-U = 1e3 * unp.uarray(U, U_err)
+x, F = np.genfromtxt('data.txt', unpack=True)
+
+D = F * x
+
+
 
 table_header = r'''
   \begin{tabular}{c c c}
     \toprule
-    {$/Delta x \:/\: [cm]$} & \multicolumn{2}{c}{$F \:/\: [N]$} & \multicolumn{3}{c}{$D$}
+    {$\Delta x \:/\: \si{\centi\meter}$} & {$F \:/\: \si{\newton}$} & {$D \:/\: \si{\newton\per\centi\meter\tothe{-1}}$}
     \midrule
 '''
 
 table_footer = r'''    \bottomrule
   \end{tabular}
 '''
-row_template = r'    {0:1.3f} & {1.n:1.2f} & {1.s:1.2f} & {2:1.3f} & {3.n:1.2f} & {3.s:1.2f} \\'
+row_template = r'     {0:1.1f} & {1:1.2f} & {2:1.2f}  \\'
 
 
-with open('build/loesung-table.tex', 'w') as f:
+with open('build/table.tex', 'w') as f:
     f.write(table_header)
-    for row in zip(t1, U1, t2, U2):
+    for row in zip(x, F, D):
         f.write(row_template.format(*row))
         f.write('\n')
     f.write(table_footer)
