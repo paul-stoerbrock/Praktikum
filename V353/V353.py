960b4ce35810f2a8e2ea269e0673a96f19e29ae7
Tobias Rücker <tobias.ruecker@tu-dortmund.de>
2019-11-21 00:31:19 +0100

    create header and add example to test Makefile

---
+++ V353/V353.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds
+)
+
+
+x = np.linspace(0, 1)
+plt.plot(x, x**2, 'b-')
+plt.savefig('plot.pdf')
