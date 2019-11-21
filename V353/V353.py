e1a4a63da2b5a10388ff75a7df890e040237881e
Tobias Rücker <tobias.ruecker@tu-dortmund.de>
2019-11-21 23:04:09 +0100

    implemented curve fit for non linear regression

--- V353/V353.py
+++ V353/V353.py
@@ -9,29 +9,41 @@ from uncertainties.unumpy import (
 )
 from scipy import stats 
 from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
 import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
 
 U0 = 0.3 
 
 f, A, t = np.genfromtxt('data.txt', unpack=True) #Variablen definieren f=Frequenz, A=Amplitude, t=Zeit
 
-def f(f, A, x, t):
-    return [...] * np.exp(xt)
+def g(f, RC):
+    return  1/(np.sqrt(4 * (np.pi)**2 * f**2 * RC**2+1))
 
 A0 = A / U0
-#params, cov_mat = np.polyfit(f, A0, deg=1, cov=True)
-slope, intercept, r_value, p_value, std_err = stats.linregress(f, A0)
+
+
+popt, pcov = curve_fit(
+    g,
+    f,
+    A0,
+    sigma=None,
+    absolute_sigma=True,
+    p0=[1e-03]
+    )
+print(popt) #Überprüfung von Größe RC
+
 plt.plot(f, A0, 'k.', label="Messwerte")
 plt.yscale('log')
 
-x_plot = np.linspace(0, 100)
+x_plot = np.linspace(0, 100000, 100000)
 
 
 
-plt.plot(x_plot, y_plot, 'r-', label="Nicht-lineare Regression")
+plt.plot(x_plot, g(x_plot,*popt), 'r-', label="Nicht-lineare Regression")
 
 plt.legend(loc="best")
 plt.title('Messwerte + Linear Regression')
 plt.xlabel('Frequenz in Hertz')
-plt.ylabel('Amplitude in Volt')
+plt.ylabel('A/U0 in Volt')
+plt.tight_layout
 plt.savefig('build/plot.pdf')
