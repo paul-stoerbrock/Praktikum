a4e30b78d0ac38fa958213960f9073edc1afc992
Tobias Rücker <tobias.ruecker@tu-dortmund.de>
2019-11-29 00:22:33 +0100

    calculated relative error

--- V354/V354.py
+++ V354/V354.py
@@ -24,60 +24,135 @@ f, A, t = np.genfromtxt('data.txt', unpack=True) # f=Frequenz, A=Amplitudenspann
 
 U0 = 10 # angelegte Spannung in Volt
 
+Umax= 13  # Resonanzamplitude
+
 A0 = A/U0
 
 phi = f * t * 2 * np.pi # Umrechnung von t in phi
 
+R= 2.9 * 1e03      # Widerstand für aperiodischen Grenzfall
 
-#Plot für 4a) #################################################################################
+nuGraph = 14.9*1e03 # nu+ - nu- Breite der Resonanzkurve
+
+nu1 = 3.2 
+
+nu2 = 4.43
+
+nures = 3.74
+
+# Literaturwerte ##########################################################################################
+
+L= ufloat(3.5,0.01)*1e-03
+R1= ufloat(30.3, 0.1)
+R2= ufloat(271.6,0.2)
+C= 5.01 * 1e-09
+
+# Berechnung R_ap aus Literaturwerten
+
+R_aplit= unp.sqrt(4*L/C)
+
+# Relativer Fehler R
+
+RF_R = (R-R_aplit)/R_aplit
+
+# Berechnung von nu+ - nu-
+
+nuRech= (R2)/(2 * np.pi * L)
+
+# Relativer Fehler nu+ - nu-
+
+RF_nu= (nuGraph-nuRech.n)/nuRech.n
+
+
+
+#Plot für a) #################################################################################
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(ta, np.log(Aa))
 
 plt.plot(ta*1e+04,np.log(Aa), 'kx', label="Messdaten")
 x_plot = np.linspace(0, 2e-04, 10)
 plt.plot(x_plot*1e04,intercept+slope*x_plot, 'r-', label="Lineare Regression")
 plt.legend(loc="best")
-plt.xlabel('Zeit in s')
-plt.ylabel('Log(A) in Volt')
+plt.xlabel('t / s')
+plt.ylabel(r'$\log (A) \:/\: $V')
+plt.grid()
 plt.tight_layout
-plt.title('Plot für Aufg. 4a)')
 plt.savefig('build/plota.pdf')
 plt.close()
 
+#Plot für c) #######################################################################################################
 
-# Erstellung des Plots c in ln-Darstellung
+# Erstellung des Plots c) in ln-Darstellung
 
 plt.plot(f, A0, 'rx', label="Messdaten")
 plt.xscale('log')
-plt.xlabel('Frequenz in Hertz')
-plt.ylabel('$U_C/U_0$ in Volt')
+plt.xlabel(r'$\nu \:/\: $Hz')
+plt.ylabel(r'$U_C/U_0 \:/\: $V')
+plt.grid()
 plt.tight_layout
 plt.savefig('build/plotcln.pdf')
 plt.close()
 
-# Erstellung des Plots c in linearer Darstellung
+# Erstellung des Plots c) in linearer Darstellung
 
-plt.plot(f, A0, 'rx', label="Messdaten")
+plt.plot(f*1e-03, A0, 'rx', label="Messdaten")
 plt.legend(loc="best")
-plt.xlabel('Frequenz in Hertz')
-plt.ylabel('$U_C/U_0$ in Volt')
+plt.axhline(Umax/(U0*np.sqrt(2)), linestyle='--') 
+plt.axvline(27.8,linestyle='--') # nu-
+plt.axvline(42.7,linestyle='--') # nu+
+plt.xlabel(r'$\nu \:/\: $kHz')
+plt.ylabel(r'$U_C/U_0 \:/\: $V')
+plt.grid()
 plt.tight_layout
 plt.savefig('build/plotclin.pdf')
 plt.close()
 
 
-# Plot d) ###########################################################################################
+# Plot für d) ###########################################################################################
 
-# Erstellung des Plots d) linear
+# Erstellung des Plots d) in ln-Darstellung
 
 plt.plot(f*1e-04, phi, 'kx', label="Messdaten")
 plt.xscale('log')
-plt.yscale('log')
-plt.axis([0, 5, 0, np.pi])
+plt.yticks( [0,np.pi/4 ,np.pi/2,3 * np.pi/4 ,np.pi],
+            [r'$0$' ,r'$+\pi/4$' ,r'$+\pi/2$' ,r'$+3\pi/4$'  ,r'$+\pi$']
+    )
 plt.legend(loc="best")
-plt.xlabel(f'$\nu$ / $10^4 Hz$')
-plt.ylabel('Phi in Bogenmaß')
+plt.xlabel(r'$\nu \:/\: 10^4$Hz')
+plt.ylabel(r'$\phi \:/\: $rad')
 plt.tight_layout
-plt.savefig('build/plotd.pdf')
+plt.grid()
+plt.savefig('build/plotdln.pdf')
 plt.close()
+
+#Erstellung des Plots d) in linearer Darstellung
+
+plt.plot(f*1e-04, phi, 'kx', label="Messdaten")
+plt.xscale('log')
+plt.yticks( [0,np.pi/4 ,np.pi/2,3 * np.pi/4 ,np.pi],
+            [r'$0$' ,r'$+\pi/4$' ,r'$+\pi/2$' ,r'$+3\pi/4$'  ,r'$+\pi$']
+    )
+plt.xticks( [3 ,4 ],
+            [3,4]
+)
+plt.legend(loc="best")
+plt.xlabel(r'$\nu \:/\: 10^4$ Hz')
+plt.ylabel(r'$\phi \:/\: $rad')
+plt.axvline(3.2,linestyle='--')   # nu1
+plt.axvline(3.74,linestyle='--')  # nures
+plt.axvline(4.43,linestyle='--')  # nu2
+plt.grid()
+plt.tight_layout
+plt.savefig('build/plotdlin.pdf')
+plt.close()
+
+
+# SI-Einheiten ####################################################################################
+
+
+
+
+
 
 # Erstellung Tabelle c), d) ###################################################################################
 
@@ -114,3 +189,9 @@ with open('build/table_c.tex', 'w') as h:
 
 # Kontrollprints
 
+print(R_aplit)
+print(RF_R)
+print(np.exp(1)**intercept)
+print(nuRech)
+print(nuGraph)
+print(RF_nu)
