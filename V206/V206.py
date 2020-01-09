0a6151ac0ed9bfa31dc362fe9a0c629028e39d88
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-01-09 10:10:44 +0100

    added .py file + header

---
+++ V206/V206.py
@@ -0,0 +1,13 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from scipy import stats 
+from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
+import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
