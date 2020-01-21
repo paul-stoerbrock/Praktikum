c371224493a4c3ff94be2035a7a116debd849d11
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-01-21 16:36:57 +0100

    added .py header

---
+++ V311/V311.py
@@ -0,0 +1,26 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from scipy import stats 
+from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
+import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+
+# Funktionsdefinitionen ########################################################################################################################################################
+
+def make_SI(num, unit, exp='', figures=None):
+    ''' Format an uncertainties ufloat as a \SI quantity '''
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ''
+        x = '{0:.{1:}uf}'.format(num, figures).replace('/', '')
+    else:
+        x = '{0:.{1:}f}'.format(num , figures)
+
+    return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
