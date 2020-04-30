8b0df04033c31fc5f4b889b86762087dc2ba4f23
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-04-30 14:05:22 +0200

    added .py and exercise 2a) to .tex

---
+++ ATP_Übung/Übungsblatt_1/Übungsblatt_1.py
@@ -0,0 +1,59 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from scipy import stats 
+from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
+import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+
+def make_SI(num, unit, exp='', figures=None):
+    ''' Format an uncertainties ufloat as a \SI quantity '''
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ''
+        x = '{0:.{1:}uf}'.format(num, figures).replace('/', '')
+    else:
+        x = '{0:.{1:}f}'.format(num, figures)
+
+    return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
+
+def A_S(R_S):
+    return 4*np.pi*R_S**2
+
+def A_E(R_E):
+    return 4*np.pi*R_S**2
+
+def P_S(A_S, T_S):
+    return sigma*A_S*T_S**4
+
+T_S = 5780
+R_S = 6.69*10**5
+R_E = 6360
+
+
+
+# tex file of T_S
+
+with open('build/T_S.tex', 'w') as f: 
+  f.write(make_SI(T_S, r'\kelvin' ,figures=0))
+
+# tex file of R_S
+
+with open('build/R_S.tex', 'w') as f: 
+  f.write(make_SI(R_S, r'\kilo\meter' ,figures=0))
+
+# tex file of R_E
+
+with open('build/R_E.tex', 'w') as f: 
+  f.write(make_SI(R_E, r'\kilo\meter' ,figures=0))
+
+# tex file of P_S
+
+with open('build/P_S.tex', 'w') as f: 
+  f.write(make_SI(P_S, r'\watt' ,figures=0))
