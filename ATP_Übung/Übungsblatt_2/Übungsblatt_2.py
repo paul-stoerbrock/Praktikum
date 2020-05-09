8c4885321ff18deed35214be4e05d722df8b75b6
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-05-09 22:31:20 +0200

    added solution to ex.4d

--- ATP_Übung/Übungsblatt_2/Übungsblatt_2.py
+++ ATP_Übung/Übungsblatt_2/Übungsblatt_2.py
@@ -10,7 +10,9 @@ from uncertainties.unumpy import (
 from scipy import stats 
 from scipy.stats import sem #standard error of mean = sem(x)
 from scipy.optimize import curve_fit #function curve_fit 
+from astropy import constants as astro
 import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+
 
 def make_SI(num, unit, exp='', figures=None):
     ''' Format an uncertainties ufloat as a \SI quantity '''
@@ -22,3 +24,15 @@ def make_SI(num, unit, exp='', figures=None):
         x = '{0:.{1:}f}'.format(num, figures)
 
     return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
+
+M_stern = 0.083*1.98840987*1e+30
+
+#Eddington Leuchtkraft:
+L = const.g*4*np.pi*M_stern*const.c/0.02
+
+# tex file for L
+
+with open('build/L.tex', 'w') as f:
+  f.write(make_SI(L*1e-30,r'\watt', figures=2))
+
+print(astro.M_sun)
