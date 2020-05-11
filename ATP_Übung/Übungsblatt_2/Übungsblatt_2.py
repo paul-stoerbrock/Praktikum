e1851ef4254f5932d7eb31ac29ed5a612896ae3b
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-05-11 12:28:23 +0200

    merging files

--- ATP_Übung/Übungsblatt_2/Übungsblatt_2.py
+++ ATP_Übung/Übungsblatt_2/Übungsblatt_2.py
@@ -25,14 +25,26 @@ def make_SI(num, unit, exp='', figures=None):
 
     return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
 
+R_S = 6.96*10**8                    #Radius in m
+A_S = 4*np.pi*R_S**2
+T_S = 5780                          #Temperatur in K
+P_S = const.sigma*A_S*T_S**4
+
 M_stern = 0.083*1.98840987*1e+30
 
 #Eddington Leuchtkraft:
 L = const.G*4*np.pi*M_stern*const.c/0.02
 
+P_sol = P_S/L
+
 # tex file for L
 
 with open('build/L.tex', 'w') as f:
   f.write(make_SI(L*1e-30,r'\watt', figures=2))
+
+# tex file for P_sol
+
+with open('build/P_sol.tex', 'w') as f:
+  f.write(make_SI(P_sol,r'', figures=5))
 
 print(astro.M_sun)
