35f3201eea0a2ee76a950488dacdb83adc523028
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-05-18 17:02:24 +0200

    added structure

---
+++ ATP_Übung/Übungsblatt_3/Übungsblatt_3.py
@@ -0,0 +1,31 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from scipy import stats 
+from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
+from astropy import constants as astro
+import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+
+
+def make_SI(num, unit, exp='', figures=None):
+    ''' Format an uncertainties ufloat as a \SI quantity '''
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ''
+        x = '{0:.{1:}uf}'.format(num, figures).replace('/', '')
+    else:
+        x = '{0:.{1:}f}'.format(num, figures)
+
+    return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
+
+## tex file for L
+#
+#with open('build/L.tex', 'w') as f:
+#  f.write(make_SI(L*1e-30,r'\watt', figures=2))
