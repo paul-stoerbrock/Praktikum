2636afe2d02c2885d947492847ee3dd6ef751b8d
Tobias Rücker <tobias.ruecker@tu-dortmund.de>
2020-05-21 07:48:40 +0200

    added elements of Übungsblatt 5

---
+++ Physik_Übung/Übungsblatt_5/Übungsblatt_5.py
@@ -0,0 +1,31 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from scipy import stats 
+from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
+import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+
+# Funktionsdefinitionen ########################################################################################################################################################
+
+def make_SI(num, unit, exp='', figures=None):
+    ''' Format an uncertainties ufloat as a \SI quantity '''
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ''
+        x = '{0:.{1:}uf}'.format(num, figures).replace('/', '')
+    else:
+        x = '{0:.{1:}f}'.format(num , figures)
+
+    return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
+
+
+
+print(np.exp(4 * np.sqrt(2 *const.m_e *(4.5*const.e)**3)/(3*const.hbar*5e09*const.e)))
+print(const.m_e)
