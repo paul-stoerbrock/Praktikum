2292a4910986fe7161b31f82d8b8a029e13504b4
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-05-21 20:11:09 +0200

    added solution to ex. 1)

--- Physik_Übung/Übungsblatt_5/Übungsblatt_5.py
+++ Physik_Übung/Übungsblatt_5/Übungsblatt_5.py
@@ -26,6 +26,20 @@ def make_SI(num, unit, exp='', figures=None):
     return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
 
 
+P_1 = np.exp(-4 * np.sqrt(-2 *const.m_e *(-4.5*const.e)**3)/(3*const.hbar*5e08*const.e))
+P_2 = np.exp(-4 * np.sqrt(-2 *const.m_e *(-4.5*const.e)**3)/(3*const.hbar*5e09*const.e))
 
-print(np.exp(4 * np.sqrt(2 *const.m_e *(4.5*const.e)**3)/(3*const.hbar*5e09*const.e)))
-print(const.m_e)
+# tex file for P_1
+
+with open('build/P_1.tex', 'w') as f:
+  f.write(make_SI(P_1*1e+57,r'\percent', exp='1e-57', figures=1))
+
+# tex file for P_2
+
+with open('build/P_2.tex', 'w') as f:
+  f.write(make_SI(P_2*1e+06,r'\percent', exp='1e-06', figures=1))
+
+
+
+print(P_1)
+print(P_2)
