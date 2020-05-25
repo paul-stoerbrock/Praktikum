f6ca19dd2721cf9fdb3cbe35455ac3aeff1f8fea
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-05-25 22:01:34 +0200

    added solution to ex. 11

--- ATP_Übung/Übungsblatt_4/Übungsblatt_4.py
+++ ATP_Übung/Übungsblatt_4/Übungsblatt_4.py
@@ -12,6 +12,7 @@ from scipy.stats import sem #standard error of mean = sem(x)
 from scipy.optimize import curve_fit #function curve_fit 
 from astropy import constants as astro
 import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+import math
 
 
 def make_SI(num, unit, exp='', figures=None):
@@ -25,4 +26,17 @@ def make_SI(num, unit, exp='', figures=None):
 
     return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
 
+M = -26.74-5*math.log10(149.6*10**6/(10*3.086*1e+13))
 
+r = 10**((-1.46-1.43)/5+1)
+print(r)
+
+# tex file for M
+
+with open('build/M.tex', 'w') as f:
+  f.write(make_SI(M,r'', figures=4))
+
+# tex file for r
+
+with open('build/r.tex', 'w') as f:
+  f.write(make_SI(r,r'pc', figures=4))
