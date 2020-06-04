b4b0bfe27d62d02ceeede8443d70e9c1bfc54aaf
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-06-04 19:26:28 +0200

    added structure

---
+++ Physik_Übung/Übungsblatt_7/Übungsblatt_7.py
@@ -0,0 +1,31 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from scipy import stats 
+from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
+import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+
+# Funktionsdefinitionen ########################################################################################################################################################
+
+def make_SI(num, unit, exp='', figures=None):
+    ''' Format an uncertainties ufloat as a \SI quantity '''
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ''
+        x = '{0:.{1:}uf}'.format(num, figures).replace('/', '')
+    else:
+        x = '{0:.{1:}f}'.format(num , figures)
+
+    return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
+
+## tex file for P_1
+#
+#with open('build/P_1.tex', 'w') as f:
+#  f.write(make_SI(P_1*1e+57,r'\percent', exp='1e-55', figures=1))
