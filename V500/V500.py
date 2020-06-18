6ba1e6142f6e28cd0d4899a2b58159be2ec7aba8
Paul Störbrock <paul.stoerbrock@tu-dortmund.de>
2020-06-18 14:35:06 +0200

    added Makefile, .py, .tex

---
+++ V500/V500.py
@@ -0,0 +1,32 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from scipy import stats 
+from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
+import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+
+def make_SI(num, unit, exp='', figures=None):
+    ''' Format an uncertainties ufloat as a \SI quantity '''
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ''
+        x = '{0:.{1:}uf}'.format(num, figures).replace('/', '')
+    else:
+        x = '{0:.{1:}f}'.format(num, figures)
+
+    return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
+
+
+
+
+## tex file for 
+#
+#with open('build/.tex', 'w') as f:
+#  f.write(make_SI(,r'', figures=1))
