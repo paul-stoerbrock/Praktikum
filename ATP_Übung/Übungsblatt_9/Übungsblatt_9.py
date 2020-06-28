a0cd6cee8b9c2251d6090761aecd5fb20a0fe50e
Tobias Rücker <tobias.ruecker@tu-dortmund.de>
2020-06-28 23:38:51 +0200

    added structure of ex 9 and added 27 a)

---
+++ ATP_Übung/Übungsblatt_9/Übungsblatt_9.py
@@ -0,0 +1,36 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+from scipy import stats 
+from scipy.stats import sem #standard error of mean = sem(x)
+from scipy.optimize import curve_fit #function curve_fit 
+from astropy import constants as astro
+import scipy.constants as const #Bsp.: const.physical_constants["proton mass"], output -> value, unit, error
+import math
+
+
+def make_SI(num, unit, exp='', figures=None):
+    ''' Format an uncertainties ufloat as a \SI quantity '''
+    if np.any(stds([num])):
+        if figures is None:
+            figures = ''
+        x = '{0:.{1:}uf}'.format(num, figures).replace('/', '')
+    else:
+        x = '{0:.{1:}f}'.format(num, figures)
+
+    return r'\SI{{{}{}}}{{{}}}'.format(x, exp, unit)
+
+
+
+M = np.sqrt((549*const.c**6)/(500*np.pi*1408*const.G**3) )/astro.M_sun.value
+
+
+
+with open('build/M_BH.tex', 'w') as f:
+  f.write(make_SI(M*1e-08,r'M_{\odot}',exp='e08' ,figures=2))
