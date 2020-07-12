8ab81a446e86ea76ae413a0fb9c8d032794d7269
Tobias Rücker <tobias.ruecker@tu-dortmund.de>
2020-07-12 23:27:29 +0200

    added tables, new command leftside, literature

--- V601/V601.py
+++ V601/V601.py
@@ -44,7 +44,19 @@ def error(f, err_vars=None):
 def rel_err(mess, lit):
     return abs((unp.nominal_values(mess)-unp.nominal_values(lit))/unp.nominal_values(lit))*100
 
+def m( U, I):
+    m_T = np.array([])
+    x=0
+    while x < len(U)-1:
+        m_T = np.append(m_T, (I[x+1]-I[x])/(U[x+1]-U[x]))
+        x=x+1    
+    return m_T      # Funktion zur automatischen Berechnung der Steigung für die differentielle Energieverteilung der Elektronen
 
+def p(T):
+    return 5.5*10**7 *np.exp(-6876/T)
+
+def w(T):
+    return 0.0029/p(T)
 
 # Messwerte ######################################################################
 
@@ -59,41 +71,52 @@ U_B = 11
 T1_UB = const.convert_temperature( 164, 'C' ,'K')
 T2_UB = const.convert_temperature( 175, 'C' ,'K')
 U_A = 1
+print(T1_UB)
+print(T2_UB)
 
+m_T1 = m(UA_T1, IA_UA_T1)
+m_T2 = m(UA_T2, IA_UA_T2)
 
+# Berechnung des Wirkungsquerschnitt
 
+T = np.array([T1_UA, T2_UA, T1_UB, T2_UB])
+p_sat = p(T)
+w_bar = w(T)
+print(T)
+print(p_sat)
+print(w_bar)
 
 # Plots ####################################################################
 
 
 # Plot für die Steigung der Gegenspannung %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-par1, cov1 = np.polyfit( UA_T1, IA_UA_T1, deg=1, cov=True )
+#par1, cov1 = np.polyfit( UA_T1, IA_UA_T1, deg=1, cov=True )
+#
+#err1 = np.sqrt(np.diag(cov1))
+#
+#par2, cov2 = np.polyfit( UA_T2, IA_UA_T2, deg=1, cov=True )
+#
+#err2 = np.sqrt(np.diag(cov2))
 
-err1 = np.sqrt(np.diag(cov1))
 
-par2, cov2 = np.polyfit( UA_T2, IA_UA_T2, deg=1, cov=True )
-
-err2 = np.sqrt(np.diag(cov2))
-
-
-plt.plot(UA_T1, IA_UA_T1 ,'kx' , label='Messwerte bei 25,3°C')
-plt.plot(UA_T2, IA_UA_T2 ,'bx' , label='Messwerte bei 145,5°C')
-x_plot = np.linspace(0, 5, 1000)
-plt.plot(x_plot, par1[0]*x_plot+par1[1], 'r-', label='Fitgerade bei 25,3°C')
-plt.plot(x_plot, par2[0]*x_plot+par2[1], 'b-', label='Fitgerade bei 145,5°C')
+plt.plot(UA_T1[:10], m_T1 ,'kx' , label='Messwerte bei 25,3°C')
+#plt.plot(UA_T2[:5], m_T2 ,'bx' , label='Messwerte bei 145,5°C')
+#x_plot = np.linspace(0, 5, 1000)
+#plt.plot(x_plot, par1[0]*x_plot+par1[1], 'r-', label='Fitgerade bei 25,3°C')
+#plt.plot(x_plot, par2[0]*x_plot+par2[1], 'b-', label='Fitgerade bei 145,5°C')
 plt.legend(loc="best")
 plt.xlabel(r' $U_A \:/\:V$')
-plt.ylabel(r'$I_A \:/\: nA$')
+plt.ylabel(r'$m \:/\: A/V$')
 plt.grid()
 plt.tight_layout
 plt.savefig('build/plot_UA.pdf')
 plt.close()
 
 
-par_T1 = unp.uarray(par1, err1)
-
-par_T2 = unp.uarray(par2, err2)
+#par_T1 = unp.uarray(par1, err1)
+#
+#par_T2 = unp.uarray(par2, err2)
 
 
 
@@ -101,6 +124,11 @@ par_T2 = unp.uarray(par2, err2)
 
 plt.plot(UB_T1, IA_UB_T1 ,'kx' , label='Messwerte bei 164°C')
 plt.plot(UB_T2, IA_UB_T2 ,'bx' , label='Messwerte bei 175°C')
+plt.axvline(25, c='g',label='Maxima')
+plt.axvline(31, c='g')
+plt.axvline(36, c='g')
+plt.axvline(41, c='g',)
+plt.axvline(46, c='g')
 plt.legend(loc="best")
 plt.xlabel(r' $U_B \:/\:V$')
 plt.ylabel(r'$I_A \:/\: nA$')
@@ -108,3 +136,72 @@ plt.grid()
 plt.tight_layout
 plt.savefig('build/plot_UB.pdf')
 plt.close()
+
+
+
+
+# Tabellen ######################################################################
+
+
+# Tabelle der Temperaturen sowie Druck und Wirkungsquerschnitt
+
+table_header = r'''
+  \begin{longtable}[H]{S[table-format=3.2] S[table-format=5.1] S[table-format=3.1] }
+    \caption{
+    }\\ 
+    \toprule
+    \multicolumn{1}{c}{ $T\:/\:K$ } & \multicolumn{1}{c}{$p_{\text{sät}} \:/\:\mu bar$ }   &
+    \multicolumn{1}{c}{ $\bar w \:/\:\mu m$ }     \\
+    \cmidrule(lr{0,5em}){1-3} 
+
+'''
+table_footer = r'''    \bottomrule
+  \label{tab:1}
+  \end{longtable}
+'''
+row_template = r'     {0:1.1f} & {1:1.2f} & {2:1.1f}   \\'
+
+with open('build/T_table.tex', 'w') as g:
+    g.write(table_header)
+    for row in zip(T, p_sat*1e03, w_bar*1e04):
+        g.write(row_template.format(*row).replace('nan',' ').replace('.',',') )
+        g.write('\n')
+    g.write(table_footer)
+
+
+
+# Tabelle für Frank-hertz-Kurve
+
+IA_UB_T1_1, IA_UB_T1_2 = np.array_split(IA_UB_T1*1e12, 2)
+IA_UB_T2_1, IA_UB_T2_2 = np.array_split(IA_UB_T2*1e12, 2)
+UB_T1_1, UB_T1_2 = np.array_split(UB_T1, 2)
+UB_T2_1, UB_T2_2 = np.array_split(UB_T2, 2)
+
+table_header = r'''
+  \begin{longtable}[H]{S[table-format=2.0] S[table-format=3.0] S[table-format=2.0] S[table-format=3.0] S[table-format=2.0] S[table-format=3.0] S[table-format=2.0] S[table-format=3.0]  }
+    \caption{
+    }\\ 
+    \toprule
+    \multicolumn{2}{c}{T=437,15 K} & \multicolumn{2}{c}{T=448,15 K} & \multicolumn{2}{c}{T=437,15 K} & \multicolumn{2}{c}{T=448,15 K} \\
+    \cmidrule(lr{0,5em}){1-2} \cmidrule(lr{0,5em}){3-4} \cmidrule(lr{0,5em}){5-6} \cmidrule(lr{0,5em}){7-8} \\
+    \multicolumn{1}{c}{ $U_B\:/\:V$ } & \multicolumn{1}{c}{$I\:/\:pA$ }   &
+    \multicolumn{1}{c}{ $U_B\:/\:V$ } & \multicolumn{1}{c}{$I\:/\:pA$ }   &
+    \multicolumn{1}{c}{ $U_B\:/\:V$ } & \multicolumn{1}{c}{$I\:/\:pA$ }   &
+    \multicolumn{1}{c}{ $U_B\:/\:V$ } & \multicolumn{1}{c}{$I\:/\:pA$ }  \\
+    \cmidrule(lr{0,5em}){1-4} \cmidrule(lr{0,5em}){5-8}  
+
+'''
+table_footer = r'''    \bottomrule
+  \label{tab:2}
+  \end{longtable}
+'''
+row_template = r'     {0:2.0f} & {1:3.0f} & {2:2.0f} & {3:3.0f} & {4:2.0f} & {5:3.0f} & {6:2.0f} & {7:3.0f}  \\'
+
+with open('build/UB_table.tex', 'w') as g:
+    g.write(table_header)
+    for row in zip(UB_T1_1, IA_UB_T1_1, UB_T2_1, IA_UB_T2_1, UB_T1_2, IA_UB_T1_2, UB_T2_2, IA_UB_T2_2  ):
+        g.write(row_template.format(*row).replace('nan',' ').replace('.',',') )
+        g.write('\n')
+    g.write(table_footer)
+
+
